9aa6c388744606bb752a6494d2d3308c8d903aed
gabrielvieira-web <vieira.github@gmail.com>
2023-12-05 10:15:58 -0300

    Adicionando script do google analytics

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -2,7 +2,7 @@ async function listaPaises() {
     const conexao = await fetch("https://restcountries.com/v3.1/all");
     const conexaoConvertida = await conexao.json();
 
-    return conexaoConvertida;
+    return conexaoConvertida
 }
 
 async function buscaPais(termoDeBusca) {
